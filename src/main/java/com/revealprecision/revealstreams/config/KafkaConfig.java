f2d9ca040d65c28b511c94a86d99f58b80c937db
trevlenb2 <trevlenb2@gmail.com>
2022-08-17 08:49:30 +0200

    build

--- src/main/java/com/revealprecision/revealstreams/config/KafkaConfig.java
+++ src/main/java/com/revealprecision/revealstreams/config/KafkaConfig.java
@@ -1,12 +1,16 @@
 package com.revealprecision.revealstreams.config;
 
+import static org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG;
 import static org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
 import static org.apache.kafka.streams.StreamsConfig.COMMIT_INTERVAL_MS_CONFIG;
 import static org.apache.kafka.streams.StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG;
 import static org.apache.kafka.streams.StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG;
 import static org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME;
 
+import com.revealprecision.revealstreams.messaging.Message;
 import com.revealprecision.revealstreams.props.KafkaProperties;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,6 +24,7 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.config.TopicConfig;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.streams.StreamsConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -79,34 +84,13 @@ public class KafkaConfig {
   }
 
   @Bean(name = DEFAULT_STREAMS_CONFIG_BEAN_NAME)
-  KafkaStreamsConfiguration kStreamsConfig(
-      @Autowired DeadLetterPublishingRecoverer deadLetterPublishingRecoverer) {
-    Map<String, Object> props = commonprops(deadLetterPublishingRecoverer);
+  KafkaStreamsConfiguration kStreamsConfig() {
+    Map<String, Object> props = new HashMap<>();
     props.put(StreamsConfig.APPLICATION_ID_CONFIG, kafkaProperties.getApplicationId());
-    props.put(DEFAULT_VALUE_SERDE_CLASS_CONFIG, JsonSerde.class);
+    props.put(BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
     props.put(COMMIT_INTERVAL_MS_CONFIG, "5000");
     props.put(MAX_POLL_INTERVAL_MS_CONFIG,String.valueOf(45*60*1000));
     return new KafkaStreamsConfiguration(props);
   }
 
-  private Map<String, Object> commonprops(
-      DeadLetterPublishingRecoverer deadLetterPublishingRecoverer) {
-    Map<String, Object> props = new HashMap<>();
-    props.put(BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-    props.put(DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-
-    props.put(JsonDeserializer.TRUSTED_PACKAGES,
-        "com.revealprecision.revealserver.messaging.*");
-    props.put(StreamsConfig.DEFAULT_DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG,
-        RecoveringDeserializationExceptionHandler.class);
-    props.put(RecoveringDeserializationExceptionHandler.KSTREAM_DESERIALIZATION_RECOVERER,
-        deadLetterPublishingRecoverer);
-    return props;
-  }
-
-  @Bean
-  public DeadLetterPublishingRecoverer recoverer(@Autowired KafkaTemplate kafkaTemplate) {
-    return new DeadLetterPublishingRecoverer(kafkaTemplate,
-        (record, ex) -> new TopicPartition("recovererDLQ", -1));
-  }
 }
--- src/main/java/com/revealprecision/revealstreams/config/KafkaConsumerConfig.java
+++ src/main/java/com/revealprecision/revealstreams/config/KafkaConsumerConfig.java
@@ -15,40 +15,39 @@ import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.messaging.Message;
 
-@EnableKafka
-@Configuration
+
 public class KafkaConsumerConfig {
 
-  @Value(value = "${kafka.bootstrapAddress}")
-  private String bootstrapAddress;
-
-  @Value(value = "${kafka.groupId}")
-  private String groupId;
-
-  @Bean
-  public ConsumerFactory<String, Message> consumerFactory() { //TODO: consider serialization/deserialization failures
-    Map<String, Object> props = new HashMap<>();
-    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-    props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
-    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
-    props.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
-    props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
-    props.put(JsonDeserializer.VALUE_DEFAULT_TYPE,
-        "com.revealprecision.revealserver.messaging.Message");
-    props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.revealprecision");
-    props.put(JsonDeserializer.KEY_DEFAULT_TYPE, String.class);
-
-    return new DefaultKafkaConsumerFactory<>(props);
-  }
-
-  @Bean
-  public ConcurrentKafkaListenerContainerFactory<String, Message>
-  kafkaListenerContainerFactory() {
-
-    ConcurrentKafkaListenerContainerFactory<String, Message> factory =
-        new ConcurrentKafkaListenerContainerFactory<>();
-    factory.setConsumerFactory(consumerFactory());
-    return factory;
-  }
+//  @Value(value = "${kafka.bootstrapAddress}")
+//  private String bootstrapAddress;
+//
+//  @Value(value = "${kafka.groupId}")
+//  private String groupId;
+//
+//  @Bean
+//  public ConsumerFactory<String, Message> consumerFactory() { //TODO: consider serialization/deserialization failures
+//    Map<String, Object> props = new HashMap<>();
+//    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+//    props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+//    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+//    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+//    props.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
+//    props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
+//    props.put(JsonDeserializer.VALUE_DEFAULT_TYPE,
+//        "com.revealprecision.revealserver.messaging.Message");
+//    props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.revealprecision");
+//    props.put(JsonDeserializer.KEY_DEFAULT_TYPE, String.class);
+//
+//    return new DefaultKafkaConsumerFactory<>(props);
+//  }
+//
+//  @Bean
+//  public ConcurrentKafkaListenerContainerFactory<String, Message>
+//  kafkaListenerContainerFactory() {
+//
+//    ConcurrentKafkaListenerContainerFactory<String, Message> factory =
+//        new ConcurrentKafkaListenerContainerFactory<>();
+//    factory.setConsumerFactory(consumerFactory());
+//    return factory;
+//  }
 }
