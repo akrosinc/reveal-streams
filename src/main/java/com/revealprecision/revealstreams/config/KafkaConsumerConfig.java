f3c3323385b7a081b0fcc6bbc0e401fcdaf3adc3
trevlenb2 <trevlenb2@gmail.com>
2022-08-22 13:53:56 +0200

    build

--- src/main/java/com/revealprecision/revealstreams/config/KafkaConsumerConfig.java
+++ src/main/java/com/revealprecision/revealstreams/config/KafkaConsumerConfig.java
@@ -6,8 +6,8 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
@@ -15,39 +15,39 @@ import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.messaging.Message;
 
-
+@Configuration
 public class KafkaConsumerConfig {
 
-//  @Value(value = "${kafka.bootstrapAddress}")
-//  private String bootstrapAddress;
-//
-//  @Value(value = "${kafka.groupId}")
-//  private String groupId;
-//
-//  @Bean
-//  public ConsumerFactory<String, Message> consumerFactory() { //TODO: consider serialization/deserialization failures
-//    Map<String, Object> props = new HashMap<>();
-//    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-//    props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-//    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
-//    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
-//    props.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
-//    props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
-//    props.put(JsonDeserializer.VALUE_DEFAULT_TYPE,
-//        "com.revealprecision.revealserver.messaging.Message");
-//    props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.revealprecision");
-//    props.put(JsonDeserializer.KEY_DEFAULT_TYPE, String.class);
-//
-//    return new DefaultKafkaConsumerFactory<>(props);
-//  }
-//
-//  @Bean
-//  public ConcurrentKafkaListenerContainerFactory<String, Message>
-//  kafkaListenerContainerFactory() {
-//
-//    ConcurrentKafkaListenerContainerFactory<String, Message> factory =
-//        new ConcurrentKafkaListenerContainerFactory<>();
-//    factory.setConsumerFactory(consumerFactory());
-//    return factory;
-//  }
+  @Value(value = "${kafka.bootstrapAddress}")
+  private String bootstrapAddress;
+
+  @Value(value = "${kafka.groupId}")
+  private String groupId;
+
+  @Bean
+  public ConsumerFactory<String, Message> consumerFactory() { //TODO: consider serialization/deserialization failures
+    Map<String, Object> props = new HashMap<>();
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+    props.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
+    props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
+    props.put(JsonDeserializer.VALUE_DEFAULT_TYPE,
+        "com.revealprecision.revealstreams.messaging.message.Message");
+    props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.revealprecision");
+    props.put(JsonDeserializer.KEY_DEFAULT_TYPE, String.class);
+
+    return new DefaultKafkaConsumerFactory<>(props);
+  }
+
+  @Bean
+  public ConcurrentKafkaListenerContainerFactory<String, Message>
+  kafkaListenerContainerFactory() {
+
+    ConcurrentKafkaListenerContainerFactory<String, Message> factory =
+        new ConcurrentKafkaListenerContainerFactory<>();
+    factory.setConsumerFactory(consumerFactory());
+    return factory;
+  }
 }
