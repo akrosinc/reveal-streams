19b8854d0828358a9840913bd99db8ebb12b41a5
trevlenb2 <trevlenb2@gmail.com>
2025-02-22 11:01:42 +0200

    ef-mali dashboard

--- src/main/java/com/revealprecision/revealstreams/enums/ApplicableReportsEnum.java
+++ src/main/java/com/revealprecision/revealstreams/enums/ApplicableReportsEnum.java
@@ -8,9 +8,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ApplicableReportsEnum {
   IRS(List.of(ReportTypeEnum.IRS_FULL_COVERAGE.name())),
-  MDA(List.of(ReportTypeEnum.MDA_FULL_COVERAGE.name(),ReportTypeEnum.MDA_FULL_COVERAGE_OPERATIONAL_AREA_LEVEL.name(),ReportTypeEnum.ONCHOCERCIASIS_SURVEY.name())),
+  MDA(List.of(ReportTypeEnum.MDA_FULL_COVERAGE.name(),ReportTypeEnum.ONCHOCERCIASIS_SURVEY.name())),
   IRS_LITE(List.of(ReportTypeEnum.IRS_LITE_COVERAGE.name(),
-      ReportTypeEnum.IRS_LITE_COVERAGE_OPERATIONAL_AREA_LEVEL.name())),
+      ReportTypeEnum.IRS_LITE_COVERAGE_OPERATIONAL_AREA_LEVEL.name(),ReportTypeEnum.MDA_FULL_COVERAGE_OPERATIONAL_AREA_LEVEL.name())),
   MDA_LITE(List.of(ReportTypeEnum.MDA_LITE_COVERAGE.name())),
   SURVEY(List.of(ReportTypeEnum.SURVEY.name())),
   LSM(List.of(ReportTypeEnum.LSM_HOUSEHOLD_SURVEY.name(),ReportTypeEnum.LSM_HABITAT_SURVEY.name()));
