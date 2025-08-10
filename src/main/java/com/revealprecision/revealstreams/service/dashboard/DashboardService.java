ab9c580b741f9bcb00db270d260759f95c229b8e
trevlenb2 <trevlenb2@gmail.com>
2025-08-10 16:29:44 +0200

    adding column to dashboard changes nihh-gha

--- src/main/java/com/revealprecision/revealstreams/service/dashboard/DashboardService.java
+++ src/main/java/com/revealprecision/revealstreams/service/dashboard/DashboardService.java
@@ -157,7 +157,7 @@ public class DashboardService {
       PlanLocationDetails loc, String reportLevel, List<String> filters,
       String parentIdentifierString, MdaLiteReportType type) {
 
-    log.info("loc {} reportLevel {} reportType {}",loc.getLocation().getName(),reportLevel,reportTypeEnum);
+//    log.info("loc {} reportLevel {} reportType {}",loc.getLocation().getName(),reportLevel,reportTypeEnum);
 
     switch (reportTypeEnum) {
       case MDA_FULL_COVERAGE:
